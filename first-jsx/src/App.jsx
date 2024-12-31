c6daea52901af4eb915eda40e341b8d236831f2c
Shanon Giles <sgiles21@ivytech.edu>
2024-12-31 09:23:45 -0600

    First Project using JSX, Simple Header and To do list

---
+++ first-jsx/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="title">Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </>
+  );
+}
+
+export default App
